2a2671bea1ab15e6666f2c480c8d6931c456bc14
legusto <leandroacosta2008@gmail.com>
2016-12-18 23:35:38 -0200

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Cruise Oficial</title>
+	<meta name="robots" content="index, follow">
+	<meta name="description" content="Cruise Oficial">
+	<meta name="keywords" content="cruise, moda">
+	<meta name="author" content="Leandro Augusto da Costa, leandroacosta2008@gmail.com">
+	<meta name="reply-to" content="leandroacosta2008@gmail.com">
+	<meta name="url" content="http://leandroacosta.net.br">
+
+	<meta property="og:title" content="Cruise Oficial">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta name="og:image" content="">
+	<meta name="og:description" content="Cruise Oficial">
+	<meta name="og:email" content="leandroacosta2008@gmail.com">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+	<link rel="canonical" href="" />
+	<link rel="shortcut icon" type="image/ico" href="/favicon.ico">
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+</head>
+<body>
+	<header>
+		<div class="center">
+			<h1>
+				<a href="/">
+					<img src="images/shared/logo.png" alt="Logo Cruise Oficial" title="Logo Cruise Oficial">
+				</a>
+			</h1>
+			<nav>
+				<ul>
+					<li>
+						<a data-page="sobre" title="Sobre a Cruise" href="sobre/index.php">Sobre a Cruise</a>
+					</li>
+					<li>
+						<a data-page="lookbook" title="Look Book" href="lookbook/index.php">Look Book</a>
+					</li>
+					<li>
+						<a data-page="encontrar" title="Onde Encontrar" href="encontrar/index.php">Onde Encontrar</a>
+					</li>
+					<li>
+						<a data-page="press" title="Press" href="press/index.php">Press</a>
+					</li>
+					<li>
+						<a data-page="contato" title="Contato" href="contato/index.php">Contato</a>
+					</li>
+				</ul>
+			</nav>
+			<a class="follow" href="#">Follow us on Instagram</a>
+		</div>
+	</header>
