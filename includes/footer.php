f4d6454535f48f4d4d5528889021c6e0389f7d90
Leandro Costa <leandro@fillet.com.br>
2016-12-20 15:20:05 -0200

    flowtype

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 	<script src="scripts/jquery.js"></script>
+	<script src="scripts/flowtype.js"></script>
 	<script src="scripts/main.js"></script>
 </body>
 </html>
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,14 @@
 
 $(document).ready(function(){
 
+	// $('body').flowtype({ 
+	// 	minimum   : 768,
+	// 	maximum   : 1420,
+	// 	minFont   : 12,
+	// 	maxFont   : 16,
+	// 	fontRatio : 36
+	// });
+
 	// Show current page menu
 	var currPage = $('section').data('page');
 
