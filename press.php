6ac5d412216ce704641e9eba3fa227317e63daaf
Leandro Costa <leandroacosta2008@gmail.com>
2017-05-16 22:57:06 -0300

    change press info

--- press.php
+++ press.php
@@ -5,84 +5,52 @@
 		<div class="swiper-container">
 		    <div class="swiper-wrapper">
 		    	<div class="swiper-slide">
-		    		<h3>(Alto Verão) Estadão Moda e Beleza</h3>
-	    			<img src="images/press/press-alto-verao-estadao-moda-beleza.png" class="image-full" alt="(Alto Verão) Estadão Moda e Beleza">
+		    		<h3>Revista Harper's Bazaar - Camisa Cruise em pauta de consumo</h3>
+	    			<img src="images/press/image-1.jpg" class="image-full" alt="Revista Harper's Bazaar - Camisa Cruise em pauta de consumo">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>(Alto Verão) Revista Estilo</h3>
-	    			<img src="images/press/press-alto-verao-revista-estilo.png" class="image-full" alt="(Alto Verão) Revista Estilo">
+					<div class="swiper-slide">
+		    		<h3>Revista Glamour - Camisa e calça Cruise em editorial</h3>
+	    			<img src="images/press/image-2.jpg" class="image-full" alt="Revista Glamour - Camisa e calça Cruise em editorial">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Bazaar Kids</h3>
-	    			<img src="images/press/press-bazaar-kids.jpg" class="image-full" alt="Bazaar Kids">
+					<div class="swiper-slide">
+		    		<h3>Vogue Fashion Tour</h3>
+	    			<img src="images/press/image-3.jpg" class="image-full" alt="Vogue Fashion Tour">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Bazaar - Cruise</h3>
-	    			<img src="images/press/press-bazzar-cruise.png" class="image-full" alt="Bazzar - Cruise">
+					<div class="swiper-slide">
+		    		<h3>Instagram Bazaar</h3>
+	    			<img src="images/press/image-4.jpg" class="image-full" alt="Instagram Bazaar">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>circolare</h3>
-	    			<img src="images/press/press-circolare.png" class="image-full" alt="circolare">
+					<div class="swiper-slide">
+		    		<h3>Site Vogue</h3>
+	    			<img src="images/press/image-5.jpg" class="image-full" alt="Site Vogue">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Coluna da Sonia Racy - Estadão</h3>
-	    			<img src="images/press/press-coluna-da-sonia-racy-estadao.jpg" class="image-full" alt="Coluna da Sonia Racy - Estadão">
+					<div class="swiper-slide">
+		    		<h3>Glamurama</h3>
+	    			<img src="images/press/image-6.jpg" class="image-full" alt="Glamurama">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Dressing - Fernanda Motta</h3>
-	    			<img src="images/press/press-dressing-fernanda-motta.png" class="image-full" alt="Dressing - Fernanda Motta">
+					<div class="swiper-slide">
+		    		<h3>Site RG</h3>
+	    			<img src="images/press/image-7.jpg" class="image-full" alt="Site RG">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Dressing - Lala Rudge - Paris Fashion Week</h3>
-	    			<img src="images/press/press-dressing-lala-rudge-paris-fashion-week.png" class="image-full" alt="Dressing - Lala Rudge - Paris Fashion Week">
+					<div class="swiper-slide">
+		    		<h3>Ivete Sangalo - 08.03.2017</h3>
+	    			<img src="images/press/image-8.jpg" class="image-full" alt="Ivete Sangalo - 08.03.2017">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Dressing - Lu Tranchesi - Milan Fashion Week</h3>
-	    			<img src="images/press/press-dressing-lu-tranchesi-milan-fashion-week.png" class="image-full" alt="Dressing - Lu Tranchesi - Milan Fashion Week">
+					<div class="swiper-slide">
+		    		<h3>Taís Araújo - 23.03.2017</h3>
+	    			<img src="images/press/image-9.jpg" class="image-full" alt="Taís Araújo - 23.03.2017">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Estilo</h3>
-	    			<img src="images/press/press-estilo.png" class="image-full" alt="Estilo">
+					<div class="swiper-slide">
+		    		<h3>Thássia Naves - 28.03.2017 - Gravação com Fernanda Souza</h3>
+	    			<img src="images/press/image-10.jpg" class="image-full" alt="Thássia Naves - 28.03.2017 - Gravação com Fernanda Souza">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Glamour</h3>
-	    			<img src="images/press/press-glamour.jpg" class="image-full" alt="Glamour">
+					<div class="swiper-slide">
+		    		<h3>Isabella Fiorentino 10.03.2017 - SPFW</h3>
+	    			<img src="images/press/image-11.jpg" class="image-full" alt="Isabella Fiorentino 10.03.2017 - SPFW">
 		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Revista Estilo - Editorial</h3>
-	    			<img src="images/press/press-revista-estilo-editorial.jpg" class="image-full" alt="Revista Estilo - Editorial">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Revista Estilo Camisas</h3>
-	    			<img src="images/press/press-revista-estilo-camisas.jpg" class="image-full" alt="Revista Estilo Camisas">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Revista Glamour - Editorial</h3>
-	    			<img src="images/press/press-revista-glamour-editorial.jpg" class="image-full" alt="Revista Glamour - Editorial">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Revista Topview</h3>
-	    			<img src="images/press/press-revista-topview.jpg" class="image-full" alt="Revista Topview">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Revista Vogue calça</h3>
-	    			<img src="images/press/press-revista-vogue-calca.jpg" class="image-full" alt="Revista Vogue calça">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Revista Vogue editorial</h3>
-	    			<img src="images/press/press-revista-vogue-editorial.jpg" class="image-full" alt="Revista Vogue editorial">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Vogue - Editorial</h3>
-	    			<img src="images/press/press-vogue-editorial.jpg" class="image-full" alt="Vogue - Editorial">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>vogue - Quero Já</h3>
-	    			<img src="images/press/press-vogue-quero-ja.png" class="image-full" alt="vogue - Quero Já">
-		    	</div>
-		    	<div class="swiper-slide">
-		    		<h3>Vogue Looks com lingerie</h3>
-	    			<img src="images/press/press-vogue-looks-com-lingerie.jpg" class="image-full" alt="Vogue Looks com lingerie">
+					<div class="swiper-slide">
+		    		<h3>Lu Tranchesi - 13.03.2017 - SPFW</h3>
+	    			<img src="images/press/image-12.jpg" class="image-full" alt="Lu Tranchesi - 13.03.2017 - SPFW">
 		    	</div>
 		    </div>
 		</div>
